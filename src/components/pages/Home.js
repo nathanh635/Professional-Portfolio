3706e4938ee0d998b1e36e6c7108eef33dfb2d6b
nathanh635 <nathanh635@gmail.com>
2022-07-22 20:37:05 -0600

    updated AboutMe section

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,10 +6,18 @@ export default function Home() {
       <h2>About Me</h2>
       <p>
       I graduated with a Bachelors of Applied Sciences in Environmental Engineering from University of Waterloo in 2012.
-      I worked for 10 years in environmental consulting assessing soil and groundwater conditions at a number of sites in western Canada.
-      After some time I found that I was looking for something different.
-      So I made the decision to enroll in the University of Toronto SCS Coding Bootcamp. 
-      So far, we've learned HTML, CSS, and advanced CSS, and I used this to put together this webpage. </p>
+      I worked for 10 years in environmental consulting assessing soil and groundwater conditions at a number of sites in western Canada. </p>
+
+      <p>     
+      As a project manager, I directed a number of soil and groundwater assessment and management programs in both urban and rural settings. The projects ranged in scale from small scale to
+      annual monitoring, assessment, and remediation for clients within Calgary and energy companies at sites north of Fort McMurray.
+
+      </p>
+
+      <p>
+      After some time I found that I was looking for something different. So I made the decision to enroll in the University of Toronto SCS Coding Bootcamp. 
+      I now have a breadth of skills, both front-end (HTML, CSS, Javascript, Handlebars, and React) and back-end (NodeJS, ExpressJS, MySQL, MongoDB, GraphQL).
+      This portfolio was built using React.</p>
 
       <h2>My Aspirations</h2>
 <p>
